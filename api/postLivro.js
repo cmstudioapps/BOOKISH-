a008552c8f7018cbeed042d111027ef3e4871323
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-07-12 06:54:59 -0300

    Atualizar o postLivro.js

--- api/postLivro.js
+++ api/postLivro.js
@@ -1,7 +1,16 @@
 expor default function handler(req, res) {
 
 
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  const url = "https://feed-78c44-default-rtdb.firebaseio.com/livros";
 
 
 }
