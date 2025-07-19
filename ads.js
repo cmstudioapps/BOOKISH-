7b493ff571a06c543c3d50c2dfb98fd065a4ece8
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-07-19 11:37:53 -0300

    Atualizar o ads.js

--- ads.js
+++ ads.js
@@ -2,7 +2,7 @@ ZuniAds.init([
   {
     image: "https://i.imgur.com/tC32d5j.jpeg",       // URL da imagem do anúncio (obrigatório)
     title: "Confira nosso pacotes!",   // Título do anúncio (obrigatório)
-    description: "Desbloqueie pacotes que melhoram sua existência no app!",  // Descrição do anúncio (obrigatório)
+    description: "Desbloqueie pacotes que melhoram sua experiência no app!",  // Descrição do anúncio (obrigatório)
     link: "pacotes.html",         // URL para redirecionar ao clicar (obrigatório)
     chance: 50,                  // Chance relativa de ser exibido (opcional, padrão 1)
     
