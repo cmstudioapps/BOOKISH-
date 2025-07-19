fed108668983851689cca0906835444718c5a68e
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-07-19 11:35:08 -0300

    Criar ads.js

---
+++ ads.js
@@ -0,0 +1,12 @@
+ZuniAds.init([
+  {
+    image: "https://i.imgur.com/tC32d5j.jpeg",       // URL da imagem do anúncio (obrigatório)
+    title: "Confira nosso pacotes!",   // Título do anúncio (obrigatório)
+    description: "Desbloqueie pacotes que melhoram sua existência no app!",  // Descrição do anúncio (obrigatório)
+    link: "pacotes.html",         // URL para redirecionar ao clicar (obrigatório)
+    chance: 50,                  // Chance relativa de ser exibido (opcional, padrão 1)
+    
+    once: false                // Se true, só mostra uma vez por usuário (opcional)
+  },
+  // Outros anúncios...
+]);
