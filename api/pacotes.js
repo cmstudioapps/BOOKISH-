728c716069226b351d773f3b0de6a2c8b9fbed6f
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-07-19 10:45:28 -0300

    Update pacotes.js

--- api/pacotes.js
+++ api/pacotes.js
@@ -13,7 +13,47 @@ export default function handler(req, res) {
   }
 
   const { pacote, nome, senha, codigo } = req.body
-  const 
 
+if(!pacote || !nome || !senha || !codigo) {
+
+  return res.status().json({ok: false, message: "falta dados"})
+}
   
+  const usuarios = "https://feed-78c44-default-rtdb.firebaseio.com/users";
+  let saldoMoedas = 0
+  fetch(`${usuarios}/${nome}.json`).then(response => response.json())
+  .then(data => {
+  saldoMoedas = Number(data.moedas)
+   if(senha != data.senha) {
+   return res.status(400).json({ok: false, message: "senha nao corresponde"})
+   }
+
+   fetch(`https://caiolibs.vercel.app/api/keyChecked.js?id=lojei&chave=${codigo}`).then(respons => respons.json())
+  .then(resposta => {
+ 
+  if (resposta === "true") {
+   saldoMoedas += Number(pacote)
+     fetch(`${usuarios}/${nome}/${moedas}.json`, {
+       method: "PUT",
+       headers: {"Content-type":"application/json"},
+       body: JSON.stringify(saldoMoedas)
+       
+       
+     }).then(response => response.json())
+  .then(data => {
+
+    return res.status(200).json({ok: true, message: "pacote entregue!!"})
+    
+  })
+    
+    } else {
+
+  return res.status(404).json({ok: false, message: "codigo nao encontrado"})
+    
+    }
+   
+    
+  })
+    
+  })
 }
