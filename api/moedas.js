949973117a49aebccacf347d2de8702cb375424e
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-07-19 14:21:19 -0300

    Atualizar o moedas.js

--- api/moedas.js
+++ api/moedas.js
@@ -1,5 +1,18 @@
 export default function handler (req, res) {
 
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'GET) {
+    return res.status(405).json({ erro: "Método não permitido" });
+  }
+
+
+ 
 
 }
