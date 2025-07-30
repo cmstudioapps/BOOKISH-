2cee3d928e65421c78633f3ed58988c61ab7036c
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-07-30 07:39:42 -0300

    Update logar.js

--- api/logar.js
+++ api/logar.js
@@ -1,7 +1,8 @@
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+ res.setHeader('Content-Type', 'application/json');
+
 
   if (req.method === 'OPTIONS') {
     res.status(200).end();
