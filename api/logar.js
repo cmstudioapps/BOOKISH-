6ec6169e95ca0b32f7d8f0dab0635412f6ee5594
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-07-30 07:48:19 -0300

    Update logar.js

--- api/logar.js
+++ api/logar.js
@@ -12,7 +12,7 @@ export default function handler(req, res) {
   const url = "https://feed-78c44-default-rtdb.firebaseio.com/users";
   const { acao, nome, senha } = req.body || {}
   if (!acao || !nome || !senha) {
-    return res.status().json({message: "falta informacoes"})
+    return res.status(200).json({message: "falta informacoes"})
   }
   if (acao === "login") {
     fetch(`${url}/${nome}.json`)
