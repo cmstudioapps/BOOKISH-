b0aa2879339e187a0bc934d205d6ea3245f25484
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-08-31 10:25:02 -0300

    Update ads.js

--- ads.js
+++ ads.js
@@ -4,9 +4,18 @@ ZuniAds.init([
     title: "Confira nosso pacotes!",   // Título do anúncio (obrigatório)
     description: "Desbloqueie pacotes que melhoram sua experiência no app!",  // Descrição do anúncio (obrigatório)
     link: "pacotes.html",         // URL para redirecionar ao clicar (obrigatório)
-    chance: 30,                  // Chance relativa de ser exibido (opcional, padrão 1)
+    chance: 20,                  // Chance relativa de ser exibido (opcional, padrão 1)
     
     once: false                // Se true, só mostra uma vez por usuário (opcional)
   },
+  {
+    image: "https://ibb.co/230H5cx5",       // URL da imagem do anúncio (obrigatório)
+    title: "Há 3 anos antes de voçê...",   // Título do anúncio (obrigatório)
+    description: "A Bookish preparou uma Historia para voçẽ!!",  // Descrição do anúncio (obrigatório)
+    link: "History.html",         // URL para redirecionar ao clicar (obrigatório)
+    chance: 50,                  // Chance relativa de ser exibido (opcional, padrão 1)
+    
+    once: false                // Se true, só mostra uma vez por usuário (opcional)
+  }
   // Outros anúncios...
 ]);
