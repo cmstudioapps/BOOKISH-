6e4b124c83684d44a78426c29405b958c183a8f4
cmstudioapps <138110988+cmstudioapps@users.noreply.github.com>
2025-08-31 09:38:30 -0300

    Update History.js

--- api/History.js
+++ api/History.js
@@ -1,1 +1,35 @@
 
+export default function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ erro: "Método não permitido" });
+  }
+
+  const API_KEY = "inNJuHmF7ffkiZBxdN28";
+  const { nome, dat } = req.body;
+
+  fetch("https://api.spiderx.com.br/api/ai/gemini?api_key=inNJuHmF7ffkiZBxdN28", {
+ method: "POST",
+ headers: {
+   "Content-Type": "application/json"
+  },
+  body: JSON.stringify({
+   text: pre_prompt.trim()
+  })
+ })
+ .then((response) => response.json())
+ .then((data) => {
+
+ return res.status(200).json({resposta: data.response})
+   
+ }) 
+
+}
+      
